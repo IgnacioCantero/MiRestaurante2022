edb4cddd288854fae597e030ee327fc8b99281f5
IgnacioCantero <ignacio.cantero@solvam.es>
2022-12-01 10:02:09 +0100

    Initial commit

---
+++ src/test/java/com/mirestaurante2022/mirestaurante2022/SpringBootMirestaurante2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mirestaurante2022.mirestaurante2022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMirestaurante2022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
